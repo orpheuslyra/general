37475ffd2bd43322cddd6ac7ada25c7932dfe34e
MitsuoKoizumi <m_koizumi@yumemi.co.jp>
2025-01-27 11:07:18 +0900

    fix:指摘対応

--- src/components/news/data/newsData.ts
+++ src/components/news/data/newsData.ts
@@ -6,17 +6,7 @@ export type newsData = {
 
 export const newslist: newsData[] = [
   {
-    date: new Date(2024, 11, 1),
-    title: "スタートアップイベントにおいてプレゼンテーションを行いました。",
-  },
-  {
-    date: new Date(2024, 11, 2),
-    title: "スタートアップイベントにおいてプレゼンテーションを行いました。",
-    link: new URL("http://www.google.com"),
-  },
-  {
-    date: new Date(2024, 11, 3),
-    title: "スタートアップイベントにおいてプレゼンテーションを行いました。",
-    link: new URL("http://www.google.com"),
+    date: new Date(2025, 1, 28),
+    title: "WEBサイトを公開しました。",
   },
 ];
